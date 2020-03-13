494f4b3799c9758fafb3a5b588f7873ff8a4ee02
Bernardo Issenguel <beissenguel12@gmail.com>
2020-03-13 10:33:34 +0100

    Mudando redirecionamento para adicionar novo produto

--- resources/views/produto/listagem.blade.php
+++ resources/views/produto/listagem.blade.php
@@ -8,7 +8,7 @@
 <div class="w3-container">
   <table class="w3-table-all">
     <thead>
-      <a href="{{ action('FornecedorController@create') }}" class="" style="margin-left: 92%">
+      <a href="{{ action('ProdutoController@create') }}" class="" style="margin-left: 92%">
         <button class="w3-button w3-xlarge w3-circle w3-blue">+</button>
       </a>
       <tr class="w3-red">
