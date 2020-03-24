bb3bd0d03bf816c9bc9290c3e6f19e81f2df5479
Bernardo Issenguel <beissenguel12@gmail.com>
2020-03-25 00:07:51 +0100

    Remover atributos na tabela vendas

--- database/migrations/2020_03_06_232640_create_vendas_table.php
+++ database/migrations/2020_03_06_232640_create_vendas_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateVendasTable extends Migration
 {
@@ -17,9 +17,7 @@ class CreateVendasTable extends Migration
             $table->increments('id');
             $table->integer('user_id')->unsigned();
             $table->date('data');
-            $table->decimal('valor_recebido',10,2);
-            $table->decimal('total_compra',8,2);
-            $table->decimal('troco',8,2);
+            $table->decimal('total_compra', 8, 2);
             $table->timestamps();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->charset = 'utf8';
