b47a65c86639856c80623ee7b4888983e4d3dc75
Bernardo Issenguel <beissenguel12@gmail.com>
2020-04-05 12:35:51 +0200

    Adicionar tabela de estornos

--- database/migrations/2020_03_06_232640_create_vendas_table.php
+++ database/migrations/2020_03_06_232640_create_vendas_table.php
@@ -18,10 +18,18 @@ class CreateVendasTable extends Migration
             $table->integer('user_id')->unsigned();
             $table->date('data');
             $table->decimal('total_compra', 8, 2);
+            $table->enum('estado', ['concluida', 'por_estornar'])->default('concluida');
             $table->timestamps();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->charset = 'utf8';
             $table->collation = 'utf8_general_ci';
+        });
+
+        Schema::create('estornos', function (Blueprint $table) {
+            $table->integer('venda_id')->unsigned();
+            $table->integer('produto_id')->unsigned();
+            $table->integer('quantidade_id')->unsigned();
+            $table->timestamps();
         });
     }
 
